51ace82a2e4528fe104536a4eae2f0608186020f
phmba <pmba@ic.ufal.br>
2021-11-10 21:15:41 -0300

    Começa implementação do upload de imagem para a foto de perfil

--- app/src/main/java/com/example/tdcfirebaseapp/pages/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/tdcfirebaseapp/pages/profile/ProfileFragment.kt
@@ -1,9 +1,16 @@
 package com.example.tdcfirebaseapp.pages.profile
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.tdcfirebaseapp.databinding.FragmentProfileBinding
@@ -16,6 +23,27 @@ class ProfileFragment : Fragment() {
 
     private lateinit var mViewModel: ProfileViewModel
 
+    private lateinit var requestLauncher: ActivityResultLauncher<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestLauncher = registerForActivityResult(RequestPermission()) { isGranted ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+                Log.d(TAG, "PERMISSION_IS_GRANTED")
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // features requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+                Log.d(TAG, "PERMISSION_DENIED")
+            }
+        }
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -27,10 +55,34 @@ class ProfileFragment : Fragment() {
 
         setupLogoutButton()
         setupNameCardView()
+        setupProfileImageButton()
 
         setupViewModel()
 
         return binding.root
+    }
+
+    private fun setupProfileImageButton() {
+        val changeImageButton = binding.changeProfileImageButton
+
+        changeImageButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // You can use the API that requires the permission.
+                    Log.d(TAG, "PERMISSION_GRANTED")
+                }
+
+                else -> {
+                    Log.d(TAG, "REQUEST_LAUNCH")
+                    requestLauncher.launch(
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                }
+            }
+        }
     }
 
     private fun setupNameCardView() {
@@ -58,5 +110,9 @@ class ProfileFragment : Fragment() {
         }
 
         mViewModel.init()
+    }
+
+    companion object {
+        const val TAG = "ProfileFragment"
     }
 }
