56ed733dedbbae0ece6397818147b1c909e3000d
phmba <pmba@ic.ufal.br>
2021-11-11 19:28:56 -0300

    Configura mudança de foto do perfil do usuário

--- app/src/main/java/com/example/tdcfirebaseapp/pages/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/tdcfirebaseapp/pages/profile/ProfileFragment.kt
@@ -1,21 +1,31 @@
 package com.example.tdcfirebaseapp.pages.profile
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.tdcfirebaseapp.databinding.FragmentProfileBinding
 import com.example.tdcfirebaseapp.pages.profile.dialogfragments.EditNameModalBottomSheet
 import com.example.tdcfirebaseapp.pages.profile.viewmodels.ProfileViewModel
+import com.google.android.material.snackbar.Snackbar
+import java.lang.Exception
 
 class ProfileFragment : Fragment() {
 
@@ -24,22 +34,40 @@ class ProfileFragment : Fragment() {
     private lateinit var mViewModel: ProfileViewModel
 
     private lateinit var requestLauncher: ActivityResultLauncher<String>
+    private lateinit var cameraIntentLauncher: ActivityResultLauncher<Intent>
+
+    private lateinit var profileImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         requestLauncher = registerForActivityResult(RequestPermission()) { isGranted ->
             if (isGranted) {
-                // Permission is granted. Continue the action or workflow in your
-                // app.
                 Log.d(TAG, "PERMISSION_IS_GRANTED")
+                launchCameraIntent()
             } else {
-                // Explain to the user that the feature is unavailable because the
-                // features requires a permission that the user has denied. At the
-                // same time, respect the user's decision. Don't link to system
-                // settings in an effort to convince the user to change their
-                // decision.
                 Log.d(TAG, "PERMISSION_DENIED")
+                Toast.makeText(requireContext(), "Permission denied.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        cameraIntentLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.let { data ->
+                    val selectedImageUri = data.data
+
+                    if (selectedImageUri != null) {
+                        try {
+
+                            val inputStream = requireActivity().contentResolver.openInputStream(selectedImageUri)
+                            val bitmap = BitmapFactory.decodeStream(inputStream)
+                            profileImageView.setImageBitmap(bitmap)
+
+                        } catch (exception: Exception) {
+                            Toast.makeText(requireContext(), exception.message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
             }
         }
     }
@@ -59,6 +87,8 @@ class ProfileFragment : Fragment() {
 
         setupViewModel()
 
+        profileImageView = binding.profileImageView
+
         return binding.root
     }
 
@@ -66,19 +96,23 @@ class ProfileFragment : Fragment() {
         val changeImageButton = binding.changeProfileImageButton
 
         changeImageButton.setOnClickListener {
-            when {
+
+            when(
                 ContextCompat.checkSelfPermission(
                     requireContext(),
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ) == PackageManager.PERMISSION_GRANTED -> {
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                PackageManager.PERMISSION_GRANTED -> {
                     // You can use the API that requires the permission.
                     Log.d(TAG, "PERMISSION_GRANTED")
+                    launchCameraIntent()
                 }
 
                 else -> {
                     Log.d(TAG, "REQUEST_LAUNCH")
                     requestLauncher.launch(
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        Manifest.permission.READ_EXTERNAL_STORAGE
                     )
                 }
             }
@@ -110,6 +144,15 @@ class ProfileFragment : Fragment() {
         }
 
         mViewModel.init()
+    }
+
+    private fun launchCameraIntent() {
+        val cameraIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+
+        cameraIntentLauncher.launch(cameraIntent)
     }
 
     companion object {
