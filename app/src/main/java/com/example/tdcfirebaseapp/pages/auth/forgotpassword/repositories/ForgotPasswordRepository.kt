b33a299e88fd71857b23b415af070ace25432d85
phmba <pmba@ic.ufal.br>
2021-11-19 16:29:34 -0300

    Configura criação de conta e "esqueci a senha"

--- app/src/main/java/com/example/tdcfirebaseapp/pages/auth/forgotpassword/repositories/ForgotPasswordRepository.kt
+++ app/src/main/java/com/example/tdcfirebaseapp/pages/auth/forgotpassword/repositories/ForgotPasswordRepository.kt
@@ -1,14 +1,25 @@
 package com.example.tdcfirebaseapp.pages.auth.forgotpassword.repositories
 
 import com.example.tdcfirebaseapp.shared.contracts.ViewModelContracts
+import com.google.firebase.auth.FirebaseAuth
 import java.lang.Exception
 
 class ForgotPasswordRepository {
     object Instance {
 
+        private val mFbAuth = FirebaseAuth.getInstance()
+
         fun sendPasswordResetEmail(email: String, listener: ViewModelContracts.ResultListener) {
             if (email.isNotBlank()) {
-                listener.onSuccess()
+                // Envia email de esqueci senha para o email do usuário
+                mFbAuth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            listener.onSuccess()
+                        } else {
+                            listener.onFailure(task.exception!!)
+                        }
+                    }
             } else {
                 listener.onFailure(Exception("Email cannot be blank"))
             }
--- app/src/main/java/com/example/tdcfirebaseapp/pages/auth/signup/repositories/SignUpRepository.kt
+++ app/src/main/java/com/example/tdcfirebaseapp/pages/auth/signup/repositories/SignUpRepository.kt
@@ -1,10 +1,13 @@
 package com.example.tdcfirebaseapp.pages.auth.signup.repositories
 
 import com.example.tdcfirebaseapp.shared.contracts.ViewModelContracts
+import com.google.firebase.auth.FirebaseAuth
 import java.lang.Exception
 
 class SignUpRepository {
     object Instance {
+
+        private val mFbAuth = FirebaseAuth.getInstance()
 
         fun signUpWithEmailAndPassword(
             email: String,
@@ -12,7 +15,15 @@ class SignUpRepository {
             listener: ViewModelContracts.ResultListener
         ) {
             if (email.isNotBlank() && password.isNotBlank()) {
-                listener.onSuccess()
+                // Cria conta com email e senha
+                mFbAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            listener.onSuccess()
+                        } else {
+                            listener.onFailure(task.exception!!)
+                        }
+                    }
             } else {
                 listener.onFailure(Exception("Email and password cannot be blank"))
             }
